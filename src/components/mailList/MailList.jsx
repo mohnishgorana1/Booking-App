d499ae6a6f6ef14476e50e2c152e111bd7d2c1b9
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-26 22:20:49 +0530

    footer and mail list made

--- src/components/mailList/MailList.jsx
+++ src/components/mailList/MailList.jsx
@@ -1,9 +1,18 @@
-
+import './MailList.css'
 
 function MailList() {
   return (
-    <div>MailList</div>
-  )
+    <div className="mail">
+      <h1 className="mailTitle">Save time, save money!</h1>
+      <span className="mailDesc">
+        Sign up and we will send the best deals to you
+      </span>
+      <div className="mailInputContainer">
+        <input type="text" placeholder="Your Email" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
 }
 
-export default MailList
+export default MailList;
