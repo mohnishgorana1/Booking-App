13641f2a5f4e96a0640a4f004d6993c31c7d7c3c
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-26 21:33:01 +0530

    featured component init

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Booking</span>
+        <Link to='/' className="logo logoLink">
+          <span className="logo">Booking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
-
         </div>
       </div>
     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,18 @@
-import { Header, Navbar } from '../../components'
-import './home.css'
+import { Featured, Header, Navbar } from "../../components";
+import "./home.css";
 
 function Home() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by Property type</h1>
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
