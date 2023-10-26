5e756c050751fbed14e108ae1426df33fa3d9eb1
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-26 15:42:10 +0530

    initial page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { Home, Hotel, List } from "./pages";
 
 function App() {
-
-
   return (
     <>
- 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/hotels" element = {<List />} />
+          <Route path="/hotels/:id" element = {<Hotel />} />
+
+
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./home/Home";
+import List from "./list/List";
+import Hotel from "./hotel/Hotel";
+
+export  {
+    Home,
+    List,
+    Hotel,
+}
