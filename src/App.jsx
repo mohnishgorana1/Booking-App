e99de04bb385ad2e15b75909fdc50ad1527f6c32
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-26 22:38:57 +0530

    home done

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ function App() {
           <Route path="/" element = {<Home />} />
           <Route path="/hotels" element = {<List />} />
           <Route path="/hotels/:id" element = {<Hotel />} />
-
-
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { Featured, Header, Navbar } from "../../components";
+import { Featured, FeaturedProperties, Footer, Header, MailList, Navbar, PropertyList } from "../../components";
 import "./home.css";
 
 function Home() {
@@ -9,7 +9,13 @@ function Home() {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by Property type</h1>
-        
+        <PropertyList />
+        <h1 className="homeTitle">Home Guests Love !</h1>
+        <FeaturedProperties />
+
+        <MailList />
+        <Footer />
+
       </div>
     </div>
   );
