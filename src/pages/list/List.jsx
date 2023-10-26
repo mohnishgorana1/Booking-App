2fd91fd590a808bef2492edc2f61ddd9a5d7171d
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-26 22:38:32 +0530

    list done

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,11 +1,31 @@
 import { Header, Navbar } from "../../components"
-
+import './List.css'
 
 function List() {
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label htmlFor="">Destination</label>
+              <input type="text" />
+            </div>
+            <div className="lsItem">
+              <label htmlFor="">Check-in Date</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          <div className="listResult">
+
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
